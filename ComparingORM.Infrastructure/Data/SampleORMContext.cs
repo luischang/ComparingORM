1cc7551c368bed28896428e7b24220472efcad53
Luis Chang <luis.chang@outlook.com>
2020-12-16 10:48:23 -0500

    Cambios CNX

--- ComparingORM.Infrastructure/Data/SampleORMContext.cs
+++ ComparingORM.Infrastructure/Data/SampleORMContext.cs
@@ -22,8 +22,8 @@ namespace ComparingORM.Infrastructure.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-S1DROK0\\SQLEXPRESS;Database=SampleORM;Trusted_Connection=True;MultipleActiveResultSets=True;");
+
+                optionsBuilder.UseSqlServer("<YOUR CONNECTION STRING HERE>");
             }
         }
 
